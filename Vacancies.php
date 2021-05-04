3e4bf6c3a5acb01325912f83e8df2fe961dfa0b4
ERJennings <erjennings13@gmail.com>
2021-05-04 02:50:39 -0400

    Last Corrections

--- Vacancies.php
+++ Vacancies.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Committee List</title>
+    <title>Vacancies</title>
     <link rel="stylesheet" href="style.css">
 </head>
 
 <body class=body>
 
 <h1 class = h1>
-    All Committees
+    Committee Vacancies
 </h1>
 
 <table class = table border='1'>
@@ -20,45 +20,51 @@
         <th>Nomination Deadline</th>
     </tr>
     <tr>
-        <td>Committee on Admissions</td>
-        <td>Member</td>
-        <td>6/1/2021</td>
-    </tr>
-    <tr>
-        <td>Committee on Academic Excellence</td>
-        <td>Chair</td>
-        <td>6/5/2021</td>
-    </tr>
-    <tr>
         <td>Committee on Academic Integrity</td>
         <td>Member</td>
         <td>5/8/2021</td>
     </tr>
     <tr>
         <td>Committee of Committee Chairs</td>
-        <td>Member</td>
+        <td>Chair</td>
         <td>5/21/2021</td>
     </tr>
     <tr>
-        <td>United Faculty on COmmittees</td>
+        <td>United Faculty on Committees</td>
         <td>Member</td>
         <td>5/23/2021</td>
-    </tr>
-    <tr>
-        <td>Committee on Student Welfare</td>
-        <td>Chair</td>
-        <td>6/23/2021</td>
     </tr>
     <tr>
         <td>Committee on Community Service</td>
         <td>Member</td>
         <td>5/30/2021</td>
     </tr>
+    <tr>
+        <td>Committee on Admissions</td>
+        <td>Member</td>
+        <td>6/1/2021</td>
+    </tr>
+    <tr>
+        <td>Committee on Academic Excellence</td>
+        <td>Member</td>
+        <td>6/5/2021</td>
+    </tr>
+    <tr>
+        <td>Committee on Student Welfare</td>
+        <td>Chair</td>
+        <td>6/23/2021</td>
+    </tr>
 
 </table>
 <div class="buttonrow">
+    <form class = menubutton action="menu.php">
+        <button class = button>Main Menu</button>
+    </form>
+    <form class = menubutton action="adminmenu.php">
+        <button class = button>Admin Menu</button>
+    </form>
     <form class = menubutton action="ufocmenu.php">
-        <button class = button>Back</button>
+        <button class = button>UFOC Menu</button>
     </form>
 </div>
 
